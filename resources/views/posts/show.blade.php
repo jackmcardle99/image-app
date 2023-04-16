2fa35c2f8b38e943380204e83b37d2d582989ab7
Jack McArdle <66914827+jackmcardle99@users.noreply.github.com>
2023-04-17 00:15:06 +0100

    added x-button to comment on posts.index

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -135,11 +135,8 @@
                        value="{{$post->id}}"/>
             </div>
             <div class="flex justify-center mt-5">
-                <button type="submit" class=" inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent
-                        rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
-                        active:text-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300
-                        disabled:opacity-25 transition ease-in-out duration-150">Comment
-                </button>
+                <x-primary-button>Comment
+                </x-primary-button>
             </div>
         </form>
 
